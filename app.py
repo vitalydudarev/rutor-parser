d067c5c53df228e7775fa8a8ff58cdcf7812ee3d
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-05-24 23:21:00 +0300

    Parser improvement.

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from page_parser import PageParser
 client = HttpClient()
 start_pos = 567951
 
-for i in range(1):
+for i in range(10):
     id = start_pos + i
     link = 'http://rutor.is/torrent/' + str(id)
 
     response = client.get_response(link)
     if not response.has_error:
-        parser = PageParser(response.response_text)
+        parser = PageParser(id, response.response_text)
         valid = parser.is_valid()
         if valid:
-            parser.parse()
+            torrent_info = parser.parse()
+            print torrent_info.to_json()
+        else:
+            print str(id) + ' is invalid'
--- page_parser.py
+++ page_parser.py
@@ -2,23 +2,30 @@
 
 from lxml import html, etree
 from HTMLParser import HTMLParser
+import json
 
 
 class PageParser:
-    def __init__(self, page_str):
+    def __init__(self, page_id, page_str):
         self.__html_parser = HTMLParser()
         self.__tree = html.fromstring(page_str)
         self.__details_elem = None
+        self.__page_id = page_id
 
     def is_valid(self):
         self.__details_elem = self.__tree.xpath('//table[@id="details"]')
         return len(self.__details_elem) > 0
 
     def parse(self):
+        title = self.__tree.xpath('//div/h1/text()')[0].encode('utf-8')
         info = self.__get_info()
 
-        category = info[u'Категория'].xpath('./a/text()')
-        size = info[u'Размер'].xpath('text()')
+        category = info[u'Категория'].xpath('./a/text()')[0].encode('utf-8')
+        size = info[u'Размер'].xpath('text()')[0].encode('utf-8')
+
+        torrent_info = TorrentInfo(self.__page_id, title, category, size)
+
+        return torrent_info
 
     def __get_info(self):
         result = {}
@@ -34,3 +41,14 @@ class PageParser:
                 result[key] = value
 
         return result
+
+class TorrentInfo:
+    def __init__(self, torrent_id, title, category, size):
+        self.torrent_id = torrent_id
+        self.title = title
+        self.category = category
+        self.size = size
+
+    def to_json(self):
+        dict = {'torrent_id': torrent_id, 'title': self.title, 'category': self.category, 'size': self.size}
+        return json.dumps(dict)
